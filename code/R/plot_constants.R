362d372a0614014a863b51ac3f2d905cc17e7e4c
Martin Szyska <martin.szyska37@gmail.com>
2023-07-22 12:32:30 +0200

    update heatmap plotting function for config decoration

--- code/R/plot_constants.R
+++ code/R/plot_constants.R
@@ -8,6 +8,14 @@ cyto_colors1 <- c(
     CXCL11 = "#9b3ad7"
 )
 
+cyto_colors2 <- c(
+    wo = "white",
+    `IL2` = "blue",
+    CXCL9 = "#e1be83",
+    CXCL10 = "#de6262",
+    CXCL11 = "#9b3ad7"
+)
+
 stim_colors1 <- c(
     unstim = "white",
     stim = "green"
@@ -18,6 +26,27 @@ pop_colors1 <- c(
     `Tem+Temra` = "orange",
     `Tnaive` = "gray"
 )
+
+pop_colors2 <- c(
+    `Tscm+Tcm` = "#ba77bf",
+    `Tem+Temra` = "orange",
+    `Tnaive` = "gray"
+)
+
+
+sarah_colors = list(
+    Pop = pop_colors1,
+    Cyto = cyto_colors1,
+    Stim = stim_colors1
+)
+
+sarah_colors2 = list(
+    Pop = pop_colors2,
+    Cyto = cyto_colors2,
+    Stim = stim_colors1
+)
+
+############ GHAZAL colors ############################
 
 Cell_colors1 <- c(
     `non-Treg` = "white",
@@ -34,10 +63,4 @@ Treatment_colors1 <- c(
 ghazal_colors <- list(
     Cells = Cell_colors1,
     Treatment = Treatment_colors1
-)
-
-sarah_colors = list(
-    Pop = pop_colors1,
-    Cyto = cyto_colors1,
-    Stim = stim_colors1
 )
--- code/R/plot_heatmap.R
+++ code/R/plot_heatmap.R
@@ -6,22 +6,25 @@ library(patchwork)
 library(pheatmap)
 library(viridis)
 library(RColorBrewer)
+
+
 # source(file.path(R_path, "stat.R"))
 source(file.path(R_path, "plot_constants.R"))
-
 
 ######## FUNC :: LOAD DATA ###############
 # load the map with appropriate filtering
 filter_data <- function(
-  #  filter function should also remove
-  #  WO in case of wo columns
-  #  WO, IL-2 and CXCL9/10 in case of CXCL.. columns
-  df, 
-  max_NA=1, 
-  show_phospho_only=FALSE, 
-  show_extra_peak=FALSE,
-  ...
-  ) { # nolint
+    df, 
+    max_NA=1, 
+    show_phospho_only=FALSE, 
+    show_extra_peak=FALSE,
+    ...
+  ) { 
+    #  filter function should also remove
+    #  WO in case of wo columns
+    #  WO, IL-2 and CXCL9/10 in case of CXCL.. columns
+    
+    # nolint
     data <- df %>%
      # set the max fraction of 33 (NA) values
         filter(NAvalues < max_NA)
@@ -31,38 +34,22 @@ filter_data <- function(
             filter(!is.na(Phospho))
         }
     if (!show_extra_peak) {
-        data <- data %>% 
+        data <- data %>%
             # filter only Peak1
             filter(Peak == 1)
     }
     return(data)
 }
 
-
-df2mat <- function(df, data_col="logMFI_sc") {
-  df %>%
-    select(c(Sample, Pop, Stim, Cyto, Analyte, all_of(data_col))) %>%
-    pivot_wider(names_from = Analyte, values_from = all_of(data_col)) %>% # nolint
-    column_to_rownames(var = "Sample") %>%
-    as.matrix() %>%
-    t()
-}
-
 ######## FUNC :: CREATE HEATMAP ###############
 plot_digi_heatmap <- function(
     # maybe get rid of defaults here
-    dff,
+    data,
+    title="Digiwest Heatmap",
     data_col = "logMFI_sc_minWO",
     # provide defaults
-    cutree_rows = 3,
-    cutree_cols = 3,
-    treeheight_row = 17,
-    treeheight_col = 17,
-    show_colnames=F,
     row_info = c("Phospho", "Peak"),
     col_info = c("Cyto", "Pop", "Stim"),
-    show_stims=c("unstim", "stim"),
-    show_cytos=c("IL-2", "wo", "CXCL9", "CXCL10", "CXCL11"),
     heat_color = "heat",
     anno_colors = sarah_colors,
     show_phospho_only=TRUE,
@@ -75,39 +62,54 @@ plot_digi_heatmap <- function(
     hide_data = c(
         list(Stim = "unstim", Cyto = "wo")
         ),
+    filename=NA,
+    figsize=c(7, 7),
+    factors=list(),
     ...
   ) {
-    # reset the output
+
+    ############# SPECS ############
+    # data is a dataframe
+
+    ############ INIT #############
     while (!is.null(dev.list()))  dev.off()
-  #########################################
-    # filter for show_data entries
-    for (data in names(show_data)) {
-        # select for the filtered columns
-        dff <- dff %>% 
-            filter(.data[[data]] %in% show_data[[data]]) %>%
-            mutate({{data}} := factor(.data[[data]], levels = show_data[[data]]))
+
+    ############ FILTER ###########
+    # filter the data using the passed args
+    dff <- filter_data(data, 
+        show_phospho_only, 
+        show_extra_peak,
+        ...
+    )
+
+    # select for the show_data entries
+    for (show_col in names(show_data)) {
+        dff <- dff %>%                                     
+            filter(.data[[show_col]] %in% show_data[[show_col]]) %>%       # filter for show_data entries
+            mutate({{show_col}} := factor(.data[[show_col]], levels = show_data[[show_col]]))
+
         # reduce color annotations to filtered values
-        anno_colors[[data]] <- anno_colors[[data]][intersect(names(anno_colors[[data]]), show_data[[data]])]
+        anno_colors[[show_col]] <- anno_colors[[show_col]][intersect(names(anno_colors[[show_col]]), show_data[[show_col]])]
     }
+
     # negatively filter for the hide_data items
     for (hide_item in hide_data) {
         # build the list of filter expressions for each data of this hidden item
         filter_exprs <- c()
-        for (data in names(hide_item)) {
-            filter_exprs <- c(filter_exprs, str_glue("{data} == '{hide_item[[data]]}'"))
+        for (hide_col in names(hide_item)) {
+            filter_exprs <- c(filter_exprs, str_glue("({hide_col} == '{hide_item[[hide_col]]}')"))
         }
         filter_expr_pre <- paste(filter_exprs, collapse = ' & ')
         filter_expr <- str_glue("!({filter_expr_pre})")
-
         # evaluate the filter_expr in the filter
-        dff <- dff %>% filter(rlang::eval_tidy(rlang::parse_expr(filter_expr)))
+        dff <- dff %>%filter(rlang::eval_tidy(rlang::parse_expr(filter_expr)))
     }
 
     # get the annotations into a dataframe
     # extract the row.info
     if (show_phospho_only & !show_extra_peak) {
         # do not show any row info
-        row.info=NA
+        row_info_df=NA
     } else {
         # remove Phospho from row_info fields
         if (show_phospho_only) {
@@ -118,36 +120,43 @@ plot_digi_heatmap <- function(
             row_info <- setdiff(row_info, c("Peak"))
         }
 
-        row.info <- dff %>% 
-            select(c(Analyte, all_of(row_info))) %>% 
-            unique() %>% 
+        row_info_df <- dff %>%
+            select(c(Analyte, all_of(row_info))) %>%
+            unique() %>%
             column_to_rownames(var = "Analyte")
         if ("Phospho" %in% all_of(row_info)) {
-            row.info <- row.info %>% 
+            row_info_df <- row_info_df %>%
                 mutate(Phospho = as.factor(as.numeric(!is.na(Phospho))))
         }
         if ("Peak" %in% all_of(row_info)) {
-                row.info <- row.info %>% 
+                row_info_df <- row_info_df %>%
                 mutate(Peak = as.factor(Peak))
         }
     }
 
-    ### HARDCODED SORT
-    # dff <- dff %>%
-    #     arrange(Cells, Treatment, Donor)
+    ## apply factor ordering
+    if (length(factors) >0) {
+        for (col in names(factors)) {
+            dff <- dff %>%
+                mutate("{col}" := droplevels(factor(.data[[col]], levels=factors[[col]])))
+        }
+    }
+
+    ### dff is sorted according to the col_info list
+    dff <- dff %>%
+        arrange(across(col_info)) # arrange_(col_info)
     # if
     # extract the col.info
-    col.info <- dff %>% 
-      select(c(Sample, all_of(col_info))) %>% 
-      unique() %>% 
-      column_to_rownames(var = "Sample") %>% 
+    col_info_df <- dff %>%
+      select(c(Sample, all_of(rev(col_info)))) %>%
+      unique() %>%
+      column_to_rownames(var = "Sample") %>%
       data.frame()
 
-
-    # turn into function
-    heat.mat <- dff %>%
+    # convert to matrix
+    heat_matrix <- dff %>%
       select(c(Sample, Analyte, all_of(data_col))) %>%
-      pivot_wider(names_from = Analyte, values_from = all_of(data_col)) %>% # nolint
+      pivot_wider(names_from = Analyte, values_from = all_of(data_col)) %>%# nolint
       column_to_rownames(var = "Sample") %>%
       as.matrix() %>%
       t()
@@ -160,61 +169,26 @@ plot_digi_heatmap <- function(
             heat_color <- viridis(n = 256, alpha = 1, option = "magma")
         }
     }
+    ## adjustment for printing out
 
-    # print(heat.mat)
+    if (!is.na(filename)) {
+        if (!startsWith(filename, "/")) filename <- file.path(img_path, filename)
+        message(str_glue("Saving plot to {filename}"))
+    }
+    # print(heat_matrix)
       # call the heatmap function
-    heat.map <- heat.mat %>%
+    heatmap <- heat_matrix %>%
       pheatmap(
+        main=title,
         color = heat_color,
-        annotation_row = row.info,
-        annotation_col = col.info,
+        annotation_row = row_info_df,
+        annotation_col = col_info_df,
         annotation_colors = anno_colors,
+        filename=filename,
+        width=figsize[[2]],
+        height=figsize[[1]],
         # passing defaults
-        cutree_rows = cutree_rows,
-        cutree_cols = cutree_cols,
-        treeheight_row = treeheight_row,
-        treeheight_col = treeheight_col,
-        show_colnames = show_colnames,
         ...
       )
-    return(heat.map)
-}
-
-
-
-digi_heatmap <- function(
-    # load-plot-save combine function
-    table_file = "",
-    plot_file = "",
-    figsize = c(7, 7),
-    plot_type = "png",
-    ...
-) {
-    ## load the df
-    df <- read_tsv(table_file)
-
-    # filter the data
-    dff <- filter_data(df, ...)
-    # plot and print the data
-    if (plot_file == "") {
-        plot_file <- NA
-        width <- NA
-        height <- NA
-    } else {
-        # create the 
-        if (!endsWith(plot_file, str_glue(".{plot_type}"))) {
-            plot_file <- str_glue("{plot_file}.{plot_type}")
-        }
-        width <- figsize[[1]]
-        height <- figsize[[2]]
-    }
-    plot <- plot_digi_heatmap(
-        dff,
-        filename=plot_file,
-        width=width,
-        height=height,
-        ...
-    )
-
-    return(plot)
+    return(heatmap)
 }
